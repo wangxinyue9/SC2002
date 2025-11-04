bc1a75a74d2933bdaca200adff2a4c55d156f436
wangxinyue9 <158249266+wangxinyue9@users.noreply.github.com>
2025-11-04 10:17:08 +0800

    reorganised some stuff

--- internship_management_system/users/User.java
+++ internship_management_system/users/User.java
@@ -1,43 +1,61 @@
-package internship_management_system.users;
-
-import internship_management_system.FilterSettings;
-import internship_management_system.internships.InternshipOpportunity;
+package Assignment_1;
 import java.util.*;
 
-public abstract class User
-{
-    private String id;
+public abstract class User {
+    private String userID;
+    private String userPassword;
     private String name;
-    private String password;
-    private FilterSettings filter_settings = new FilterSettings();
 
-    public User(String id, String name, String password)
-    {
-        this.id = id;
-        this.name = name;
-        this.password = password;
+    private FilterSettings filtersettings = new FilterSettings();
+
+    public User(String name, String userID, String userPassword){
+        this.name = name; 
+        this.userID = userID;
+        this.userPassword = userPassword;
     }
 
-    public void login()
-    {
+    public void login(){
+        // NEED TO RETHINK HOW TO DO THIS IN MAIN APP
+        //right now, only checks if the password is correct
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Password: ");
-        String passwordInput = scanner.nextLine();
-
-        if (passwordInput.equals(password))
-            System.out.println("Login Successful!");
-        else
-            System.out.println("Incorrect Password! Login Unsuccessful!");
+        System.out.print("password: ");
+        String passwordInput;
+        passwordInput = scanner.nextLine();
+        if (passwordInput == userPassword){
+            System.out.println("login successful");
+        }
+        else{
+            System.out.println("incorrect password, login unsuccessful");
+        }
 
     }
 
-    public String getId() { return id; }
+    public String getUserID(){
+        return userID;
+    }
 
-    public String getName(){ return name; }
+    public String getUserPasswrd(){
+        return userPassword;
+    }
 
-    public String getPassword() { return password; }
+    public String getName(){
+        return name;
+    }
 
-    public void changePassword(String password) { this.password = password; }
+    public void changePassword(){
+        System.out.println("Old password: ");
+        Scanner scanner = new Scanner(System.in);
+        String oldPassword = scanner.nextLine();
+        if (oldPassword.equals(userPassword)){
+            System.out.println("New password: ");
+            String newPassword = scanner.nextLine();
+            userPassword = newPassword;
+        }
+        else {
+            System.out.println("Incorrect password. Failed to change password.");
+        }
+    }
+
 
     public abstract List<InternshipOpportunity> saveAndApplyFilterInternship(FilterSettings filtersettings);
 }
